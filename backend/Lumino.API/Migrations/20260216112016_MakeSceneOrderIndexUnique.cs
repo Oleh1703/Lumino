d7eeb4947fd0b16078f380106bdbb8516347f610
hitman4ik85 <hitman4ik85@gmail.com>
2026-02-16 13:22:10 +0200

    add make Scene (CourseId, Order) index unique with filter Order > 0

---
+++ backend/Lumino.API/Migrations/20260216112016_MakeSceneOrderIndexUnique.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lumino.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeSceneOrderIndexUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Scenes_CourseId_Order",
+                table: "Scenes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scenes_CourseId_Order",
+                table: "Scenes",
+                columns: new[] { "CourseId", "Order" },
+                unique: true,
+                filter: "[Order] > 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Scenes_CourseId_Order",
+                table: "Scenes");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scenes_CourseId_Order",
+                table: "Scenes",
+                columns: new[] { "CourseId", "Order" });
+        }
+    }
+}
